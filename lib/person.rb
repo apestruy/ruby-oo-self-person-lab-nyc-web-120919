61a1f065708cbb867d979438dff78e619c1a4ab7
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-11 15:11:24 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,10 @@
 # your code goes here
 class Person 
  attr_reader :name
- attr_accessor :bank_account
+ attr_accessor :bank_account, :happiness
   def initialize(name)
     @name = name 
     @bank_account = 25 
+    @happiness = 8
   end 
 end 
