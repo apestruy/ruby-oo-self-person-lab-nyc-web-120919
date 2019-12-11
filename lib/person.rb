bcd1e4fdd2f2c2244a4aeb5849c95dc9a21b00ae
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-11 15:15:10 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,18 @@
 # your code goes here
 class Person 
- attr_reader :name
- attr_accessor :bank_account, :happiness
+ attr_reader :name, :happiness
+ attr_accessor :bank_account
   def initialize(name)
     @name = name 
     @bank_account = 25 
     @happiness = 8
   end 
+  def happiness=(happiness)
+    @happiness = happiness
+    
+  end 
+  
+  
+  
+  
 end 
